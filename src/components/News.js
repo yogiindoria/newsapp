d82689ee99a36a1a6783b953c897231d6ef6acda
Yogi <yogeshindoria2000@gmail.com>
2023-02-18 15:33:20 +0530

    app cnvrtd cbc to fbc

--- src/components/News.js
+++ src/components/News.js
@@ -1,85 +1,96 @@
-import React, { Component } from 'react'
+import React, {useEffect, useState } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
+const News = (props) => {
 
-export default class News extends Component {
-
-  articles = []
-
-  static defaultProps = {
-    country: "in",
-    pageSize: 10,
-    category: "general"
-  }
-
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  }
-
-  capitaliseFunc = (string)=> {
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState()
+  const [page, setPage] = useState(1)
+  const [totalResults, setTotalResults] = useState([0])
+  
+  const capitaliseFunc = (string)=> {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
- 
-  constructor(props) {
-    super(props);
-    console.log("hello i ma constructor");
-    this.state = {
-      articles: this.articles,
-      loading: false,
-      page: 1
-    }
-    document.title = `${this.capitaliseFunc(this.props.category)} - NewsHub`;
+
+  const updateNews = async() => {
+    props.setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(70);
+    setArticles(parsedData.articles)
+    setTotalResults(parsedData.totalResults)
+    setLoading(false)
+    props.setProgress(100);
+
+    console.log(parsedData);
+   
   }
 
-  async updateNews(){
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c4fe6fd5e84147edb31e7242b44a2961&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true});
+  useEffect(() => {
+    document.title = `${capitaliseFunc(props.category)} - NewsHub`;
+    updateNews();
+  }, [])
+
+
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page +1}&pageSize=${props.pageSize}`;
+    setPage(page +1)
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState(
-      {articles: parsedData.articles, 
-      totalResults: parsedData.totalResults,
-      loading: false
-      }
-    )
-  }
+    setArticles(articles.concat(parsedData.articles))
+    setTotalResults(parsedData.totalResults)
+    console.log(parsedData); 
+  };
 
-  async componentDidMount(){
-    this.updateNews();
-  }
-
-  handleNextClick = async() => {
-    this.setState({page: this.state.page +1})
-    this.updateNews();
-  }
-
-  handlePreviousClick = async() => {
-    this.setState({page: this.state.page -1});
-    this.updateNews();
-  }
-
-  render() {
     return (
-      <div className="container my-3">
-        <h2 className="text-center" style={ { margin: '35px 0px' }}> NewsHub - Top {this.capitaliseFunc(this.props.category)} Headlines </h2>
-        {this.state.loading && <Spinner/>}
-        <div className="row my-4">
-          { !this.state.loading && this.state.articles.map( (element) => {
-            return <div className='col md-2' key={element.url}>
-              <NewsItem title={element.title?element.title.slice(0, 40):""} description={element.description?element.description.slice(0,80):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}  source={element.source.name}/>
+      <>
+        <h2 className="text-center" style={ { margin: '35px 0px' }}> NewsHub - Top {capitaliseFunc(props.category)} Headlines </h2>
+        <InfiniteScroll
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length !== totalResults}
+          loader={<Spinner/>}
+        >
+
+          <div className='container'>
+
+            <div className="row my-4">
+              {articles.map( (element) => {
+                return <div className='col md-2' key={element.url}>
+                    <NewsItem title={element.title?element.title.slice(0, 40):""} 
+                    description={element.description?element.description.slice(0,80):""} 
+                    imageUrl={element.urlToImage} 
+                    newsUrl={element.url} 
+                    author={element.author} 
+                    date={element.publishedAt}  
+                    source={element.source.name}/>
             </div>
           })}
         </div>
-        <div className="container d-flex justify-content-center">
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark mx-2 my-2" onClick={this.handlePreviousClick}> &larr; Previous </button>
-        <button disabled={this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark mx-2 my-2" onClick={this.handleNextClick}> Next &rarr; </button>
         </div>
-      </div>
+          </InfiniteScroll>
+      </>
     )
-  }
 }
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 10,
+  category: "general",
+  
+}
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+}
+
+
+export default News
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import loading from './loading.gif'
 
-export default class Spinner extends Component {
-  render() {
+ const Spinner = () => {
     return (
       <div className='text-center my-4 '>
         <img src={loading} alt="loading"/>
       </div>
     )
-  }
 }
+
+export default Spinner;
